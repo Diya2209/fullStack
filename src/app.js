e179bdd47c7bbd18c8fd4c9fd75135de5f16765d
Diya <120104383+Diya2209@users.noreply.github.com>
2024-12-07 17:30:17 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cros";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express()
@@ -9,10 +9,13 @@ app.use(cors({
     Credential: true
 }))
 
-app.use(express.json({limit : "16kb"}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
-app.use(express.urlencoded())
-app.use(express.static)
-app.use(cookieParser( ))
 
-export default app;
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/users/", userRouter)
+
+export {app}
